a857168c559bd39307c568196aac86dc3b3f7b31
zhanglongde <1039326495@qq.com>
2017-06-07 22:14:11 +0800

    1）深入浅出Node.js

--- 08-02-深入浅出nodejs/06-理解buffer/01-buffer结构/buffer对象/buffer.js
+++ 08-02-深入浅出nodejs/06-理解buffer/01-buffer结构/buffer对象/buffer.js
@@ -2,6 +2,12 @@
  * Created by Administrator on 2017/6/7.
  */
 
-(function () {
+var str = 'deep in node.js'
 
-}());
+var buf = new Buffer(str,'utf-8')
+
+console.log(buf)
+
+
+var b2 = new Buffer(100)
+console.log(b2)
